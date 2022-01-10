7fe0081496d35bc04ae7f80213797c3020c28449
Edgars <egaworld13@users.noreply.github.com>
2022-01-10 17:39:25 +0200

    Egle nesanāca

--- src/com/ega/Main.java
+++ src/com/ega/Main.java
@@ -8,42 +8,66 @@ public class Main {
 //        for (String a : stars){
 //            System.out.println(a);
 //        }
-        //1.uzd
-        for(int i = 0; i<=4; i++){
-            System.out.println("**********");
+        //1.uzd 1. variants
+        for (int i = 0; i <= 4; i++) {
+
+            System.out.print("***********");
+            System.out.println();
+        }
+
+
+        //1.uzd 2. variants.
+        for (int i = 0; i <= 4; i++) {
+            for (int a = 10; a >= 0; a--) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
         System.out.println();
         //2.uzd
-        for(int i = 0; i<=3; i++){
-            for(int a = 0; a<=i; a++) {
+        for (int i = 0; i <= 4; i++) {
+            for (int a = 0; a <= i; a++) {
                 System.out.print("*");
             }
             System.out.println();
         }
         System.out.println();
         //3.uzd
-                for(int i = 3; i>=0; i--){
-                    for(int b =0; b<= i-1; ++b)
-                        System.out.print(" ");
-            for(int a = 3; a>=i; a--) {
-                System.out.print("*");
+        for (int i = 4; i >= 0; i--) {
+            for (int b = 0; b <= i - 1; ++b)
+                System.out.print(" ");
+            for (int a = 4; a >= i; a--) {
+                System.out.print("*");// pieliekot atstarpi, sanāk egle
             }
             System.out.println();
 
         }
         System.out.println();
-        //4.uzd
-        for(int i = 3; i>=0; i--) {
-            for (int b = 0; b <= i - 1; ++b)
+        //4.uzd nesanāk galotne.vai apakša.
+        for (int i = 4; i >= 0; i--) { // rindu daudzums
+            for (int b = 0; b <= i; ++b)  // atstarpes dilstošā secībā
                 System.out.print(" ");
-            for (int a = 3; a >= i; a--) {
-                System.out.print("*");
-                for(int c = 3;)
-            }
+
+            for (int a = 4; a >= i; --a){
+
+               for (int d = 0; d+4>=a; --d) {
+                    System.out.print("*");}
+                }
+
+
+
             System.out.println();
+
         }
 
 
 
     }
-}
+    }
+
+
+
+
+
+
+
