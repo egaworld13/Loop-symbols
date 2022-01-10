7d0f71698a3af2cd2c1085046c81a92746670bb0
Edgars <egaworld13@users.noreply.github.com>
2022-01-10 18:46:42 +0200

    Gatavs!

--- src/com/ega/Main.java
+++ src/com/ega/Main.java
@@ -3,11 +3,7 @@ package com.ega;
 public class Main {
 
     public static void main(String[] args) {
-//String[] stars = {"**********"};
-//
-//        for (String a : stars){
-//            System.out.println(a);
-//        }
+
         //1.uzd 1. variants
         for (int i = 0; i <= 4; i++) {
 
@@ -24,6 +20,7 @@ public class Main {
             System.out.println();
         }
         System.out.println();
+
         //2.uzd
         for (int i = 1; i <= 5; i++) {
             for (int a = 1; a <= i; a++) {
